8192e998044fe2f846c0f1fc18e70fee015cbc84
KevinGrosso <kevin.grosso@studenti.unimi.it>
2020-11-08 17:20:15 +0100

    isPrime

---
+++ isPrime/isPrime.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(isPrime(2))
+	fmt.Println(isPrime(1))
+	fmt.Println(isPrime(4))
+	fmt.Println(isPrime(9))
+	fmt.Println(isPrime(17))
+}
+
+func isPrime(n int) bool {
+	if n > 1 {
+		var i int
+		for i = 2; i <= n/i; i++ {
+			if n%i == 0 {
+				break
+			}
+		}
+
+		if i > n/i {
+			return true
+		} else {
+			return false
+		}
+	} else {
+		return false
+	}
+}
