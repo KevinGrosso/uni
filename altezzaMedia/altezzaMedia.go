df4df0e709c633d4d38b7e1809d2177df959a4c0
KevinGrosso <kevin.grosso@studenti.unimi.it>
2020-11-11 15:25:45 +0100

    Altezza media

---
+++ altezzaMedia/altezzaMedia.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt" 
+
+// Stampa l'altezza media di un certo numero di persone
+
+func main() {
+	var quanti, tot int
+	var media float64
+	
+	fmt.Print("Inserisci il numero di persone: ")
+	fmt.Scan(&quanti)
+	
+	for i:=0; i < quanti; i++ {
+		var altezza int
+		
+		fmt.Print("Inserisci la ", i+1, "^ altezza: ")
+		fmt.Scan(&altezza)
+		
+		tot += altezza
+	}
+	
+	media = float64(tot) / float64(quanti)
+	fmt.Println("L'altezza media è", media)
+}
