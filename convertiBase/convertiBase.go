bbd3a1f1cee7dcdc26f7320882dc9524315fa50a
KevinGrosso <kevin.grosso@studenti.unimi.it>
2020-11-11 16:50:39 +0100

    Converte un numero in base 10 in un numero di un'altra base

---
+++ convertiBase/convertiBase.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var n, base int
+	var conv string
+
+	const cifre = "0123456789ABCDEF"
+
+	fmt.Print("Inserisci un numero in base 10: ")
+	fmt.Scan(&n)
+	fmt.Print("In quale base lo vuoi convertire? (da 2 a 16) ")
+	fmt.Scan(&base)
+
+	for n != 0 {
+		conv = string(cifre[n%base]) + conv
+		n /= base
+	}
+
+	fmt.Println(conv)
+}
