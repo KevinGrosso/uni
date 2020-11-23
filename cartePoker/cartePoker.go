4b33be4b54877371ca792a4f37eae3bdde42173c
KevinGrosso <kevin.grosso@studenti.unimi.it>
2020-11-23 08:55:09 +0100

    Poker

--- cartePoker/cartePoker.go
+++ cartePoker/cartePoker.go
@@ -7,42 +7,44 @@ import (
 )
 
 const (
-	numeroCarteSeme = 13
-	numeroCarte     = numeroCarteSeme * 4
+	numeroCarteSeme  = 13
+	numeroCarteMazzo = numeroCarteSeme * 4
+	numeroCarteMano  = 5
 )
 
-/* Un set è una sequenza di carte distinte */
+type mazzo [numeroCarteMazzo]int
 type set []int
 
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
 	m := mazzoNuovo(true)
+	mano := m[:numeroCarteMano]
 
-	// Stampo la mano
-	for _, c := range m[:5] {
+	//Stampo la mano
+	for _, c := range mano {
 		fmt.Printf("%s\n", strCarta(c))
+	}
+
+	if coppia(mano) {
+		fmt.Println("Coppia!")
 	}
 }
 
 func strCarta(carta int) string {
 	seme := [...]string{"cuori", "quadri", "fiori", "picche"}
-	valore := [...]string{"A", "2", "3", "4", "5", "6", "7", "8",
-		"9", "10", "J", "Q", "K"}
+	valore := [...]string{"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"}
 
-	return valore[carta%numeroCarteSeme] + " di " +
-		seme[carta/numeroCarteSeme]
+	return valore[carta%numeroCarteSeme] + " di " + seme[carta/numeroCarteSeme]
 }
 
 /* Restituisce un mazzo. Se mescolato=true, lo mescola */
-func mazzoNuovo(mescolato bool) (s set) {
-	s = make(set, numeroCarte)
-
-	for i := 0; i < numeroCarte; i++ {
+func mazzoNuovo(mescolato bool) (s mazzo) {
+	for i := 0; i < numeroCarteMazzo; i++ {
 		s[i] = i
 	}
 
 	if mescolato {
-		for i := numeroCarte; i > 0; i-- {
+		for i := numeroCarteMazzo; i > 0; i-- {
 			pos := rand.Intn(i)
 			s[pos], s[i-1] = s[i-1], s[pos]
 		}
@@ -50,5 +52,14 @@ func mazzoNuovo(mescolato bool) (s set) {
 	return
 }
 
-/* Data una mano di poker (un set di 5 carte) stabilisce (true) se c'è una coppia */
-func coppia()
+/* Dato un set di carte, stabilisce (true) se c'è almeno una coppia */
+func coppia(mano set) bool {
+	for i := 0; i < numeroCarteMano-1; i++ {
+		for j := 1; i+j < numeroCarteMano; j++ {
+			if mano[i]%numeroCarteSeme == mano[i+j]%numeroCarteSeme {
+				return true
+			}
+		}
+	}
+	return false
+}
